a52c476a57e927fc0da26d001adf11cad00bd497
Athira Pillai <athirapillai1996@yahoo.com>
2018-03-29 16:10:23 -0400

    Login added. Kinda buggy but oh well

---
+++ oto-photo-app/src/app/signup/signup.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Auth } from 'aws-amplify';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css'],
+})
+export class SignupComponent implements OnInit {
+
+  public email:string;
+  public password:string;
+  public confirmpassword:string;
+
+  constructor(public router: Router) { }
+
+  ngOnInit() {
+  }
+
+  signUpEmail() {
+   console.log(this.email);
+
+   if (this.email != "" && this.password != "" && this.confirmpassword != "" && this.password == this.confirmpassword) {
+      Auth.signUp(this.email, this.password, null, null)
+        .then((result) => {
+          console.log(result);
+          this.router.navigate(['/login']);
+        })
+        .catch((err) => console.error(err));
+   }
+  }
+}
+
